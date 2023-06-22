afdb5c844fe2d3efec69fc10b7549d33cc5c3fb7
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-22 13:05:02 +0530

    Create 11.sol

---
+++ 11.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    uint[] public array1 = [1,2,3];
+    uint[] public array2;
+    uint[10] public array3;
+    string[] public array4 = ["apple","banana","mango","lemon"];
+    string[] public array5;
+    string[10] public array6;
+}
