7c208a77add7be7b9dead9f210449046188e7ec8
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-22 14:46:13 +0530

    Create 17.sol

---
+++ 17.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    uint public value1 = 1 wei;
+    uint public value2 = 1;
+    uint public value3 = 1 gwei;
+    uint public value4 = 10000000;
+    uint public value5 = 1 ether;
+    uint public value6 = 1000000000000000000;
+}
