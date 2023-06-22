0500bb7a1a8b4acfd89fc601ac07b786fd0330ee
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-22 11:28:33 +0530

    Create 3.sol

---
+++ 3.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    string name1 = "Mainak";
+    string private name2 = "Mainak";
+    string public name3 = "Mainak";
+    string internal name4 = "Mainak";
+}
