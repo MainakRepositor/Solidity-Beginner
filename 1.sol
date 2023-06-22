59582204c772575bc06a613bccd8fbe5cd3c9501
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-22 11:09:38 +0530

    Create 1.sol

---
+++ 1.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    string public MyString = "Mainak";
+    bool public boolean1 = true;
+    uint public MyUint = 1;
+    int public MyInt = 1;
+    address public MyAddress = 0xc2fe97603151c5FfB5D37946632c0992aE7504cb;
+
+}
